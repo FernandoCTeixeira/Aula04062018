a4ea3a7490c463dd92be3496d9cd4eed0fa6822d
a <a@a-PC>
2018-06-04 20:18:13 -0300

    Complementando código com a tabela "PARKING PERIODS" banco de dados

--- ParkingWebApp/sql/db-creation-script.sql
+++ ParkingWebApp/sql/db-creation-script.sql
@@ -16,6 +16,16 @@ CREATE TABLE prices(
         (START WITH 1, INCREMENT BY 1)
     , timestamp TIMESTAMP not null
     , new_price DOUBLE PRECISION not null
+);
 
-)
+CREATE TABLE parking_periods(
+    id BIGINT not null primary key
+        GENERATED ALWAYS AS IDENTITY
+        (START WITH 1, INCREMENT BY 1)
+    , model varchar(50) not null
+    , plate varchar(7) not null
+    , begin_period TIMESTAMP not null
+    , end_period TIMESTAMP
+    , price DOUBLE PRECISION
+);
 
