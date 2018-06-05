1a1d0ca0b4867d49f4c98d33b36562947fccefbf
a <a@a-PC>
2018-06-04 22:05:45 -0300

    na classe DatabaseConnector controle de acesso, complemento com o método executeQuery

--- ParkingWebApp/src/java/br/com/fatecpg/parking/web/DatabaseConnector.java
+++ ParkingWebApp/src/java/br/com/fatecpg/parking/web/DatabaseConnector.java
@@ -32,7 +32,25 @@ public class DatabaseConnector {
         stmt.close();con.close();
     }
             
-    /*public static ArrayList<Object[]> executeQuery
-    (String SQL, Object[] parameters) throws Exception*/
+    public static ArrayList<Object[]> executeQuery
+    (String SQL, Object[] parameters) throws Exception{
+        Class.forName(DRIVER);
+        Connection con = DriverManager.getConnection(URL, USER, PASS);
+        PreparedStatement stmt = con.prepareStatement(SQL);
+        for(int i=0; i<parameters.length; i++){
+            stmt.setObject(i+1, parameters[i]);
+        }
+        ResultSet rs = stmt.executeQuery();
+        ArrayList<Object[]> list = new ArrayList<>();
+        while(rs.next()){
+            Object[] row = new Object[rs.getMetaData().getColumnCount()];
+            for(int i=1; i<=rs.getMetaData().getColumnCount();i++){
+                row[i-1] = rs.getObject(i);
+            }
+            list.add(row);
+        }
+        rs.close(); stmt.close();con.close();
+        return list;
+    }
     
 }
